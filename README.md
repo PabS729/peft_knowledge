899493d8e7296d8b7e6db4fabbeec2f2fcbb8324
Minjing <mishi@ucsd.edu>
2024-03-09 20:47:35 +0100

    v1

---
+++ README.md
@@ -0,0 +1,4 @@
+Peft Knowledge Project
+
+v1.0
+LoRA on T5 for Wikihop dataset
---
+++ lora_peft.py
@@ -0,0 +1,13 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+
+model_id = "EleutherAI/gpt-neox-20b"
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16
+)
+
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=bnb_config, device_map={"":0})
